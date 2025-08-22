ceffa644e8ad68af2a216e5dd54edb685b05393a
Thalles Passos <contato@thalles.me>
2025-08-22 01:11:19 -0300

    attempt a hotfix instead

--- src/auth-module.ts
+++ src/auth-module.ts
@@ -127,7 +127,9 @@ export class AuthModule implements NestModule, OnModuleInit {
 			// little hack to ignore any global prefix
 			// for now i'll just not support a global prefix
 			.use(`${basePath}/*path`, (req: Request, res: Response) => {
-				req.url = req.originalUrl;
+				if (req.url.startsWith(basePath)) {
+					req.url = req.originalUrl;
+				}
 
 				return handler(req, res);
 			});
